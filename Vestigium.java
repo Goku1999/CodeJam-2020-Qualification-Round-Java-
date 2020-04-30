0f7f0969d69b5042f793c2290ebdb020c3153fff
Sankalp Srivastava <32612871+Goku1999@users.noreply.github.com>
2020-04-30 18:45:55 +0530

    Update and rename Vestigium to Vestigium.java

--- Vestigium.java
+++ Vestigium.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-public class Solution {
+public class Vestigium {
   public static void main(String[] args) {
     Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
     int t = sc.nextInt();  
